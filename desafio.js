74c2da0bf090361805b7f237c16fd50019894671
vitorMMS82 <160648939+vitorMMS82@users.noreply.github.com>
2024-03-15 18:11:20 +0000

    desafio

---
+++ desafio.js
@@ -0,0 +1,20 @@
+const nota01 = 5;
+const nota02 = 4;
+const nota03 = 4;
+
+
+const media = (nota01 + nota02 + nota03) / 3
+   
+alert(`O aluno, teve a média de: ${media}`);
+
+if(media <5)
+
+{alert("o aluno foi reprovado com média inferior a 5")};
+
+if (media >= 5 && media < 6.9);
+
+{alert("o aluno ficou de recuperação pois ficou com média maior ou igual a 5 e menor que 6.9 ")}
+
+ if (media >= 7 && media <=10);
+{alert("o aluno foi aprovado com média maior ou igual a 7 ou então menor ou igual a 10")}
+
